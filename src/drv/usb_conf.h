a4c4bc07abd31cb3ba31ac2d7c3d8563386f79a7
Marcin Zapolski <mz4pol@gmail.com>
2015-01-21 22:10:10 +0100

    Fixed USB bug. Increased autopilot running frequency.

--- src/drv/usb_conf.h
+++ src/drv/usb_conf.h
@@ -59,8 +59,8 @@
 /* mask defining which events has to be handled */
 /* by the device application software */
 
-//#define IMR_MSK (CNTR_CTRM  | CNTR_SOFM  | CNTR_RESETM )
-#define IMR_MSK (CNTR_CTRM  | CNTR_WKUPM | CNTR_SUSPM | CNTR_ERRM  | CNTR_SOFM \
+#define IMR_MSK (CNTR_CTRM  | CNTR_SOFM  | CNTR_RESETM )
+//#define IMR_MSK (CNTR_CTRM  | CNTR_WKUPM | CNTR_SUSPM | CNTR_ERRM  | CNTR_SOFM \
                  | CNTR_ESOFM | CNTR_RESETM )
 
 /*#define CTR_CALLBACK*/
